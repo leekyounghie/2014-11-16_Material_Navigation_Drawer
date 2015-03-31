31249b92faf6c703e2bc6a6e96848dc2664a5cf4
leekyounghie <asianano1@gmail.com>
2015-03-31 22:52:21 +0900

    Ver003dy

--- app/src/main/java/com/androidhuman/example/materialdrawer/MainActivity.java
+++ app/src/main/java/com/androidhuman/example/materialdrawer/MainActivity.java
@@ -23,7 +23,6 @@ import com.androidhuman.example.materialdrawer.main_fragment_folder.ForuDirectio
 
 public class MainActivity extends ActionBarActivity {
     /**
-     * 최종본
      * 아래 내용은 XML파일을 인플레이션한 내용
      */
     Toolbar toolbar;
